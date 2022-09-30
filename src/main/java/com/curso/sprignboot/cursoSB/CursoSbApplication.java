926c09ce48c246634866e8b1fc7f425ab92dfee9
Agustin Saunders <saundersagustin94@gmail.com>
2022-09-29 21:30:40 -0300

    First Commit

---
+++ src/main/java/com/curso/sprignboot/cursoSB/CursoSbApplication.java
@@ -0,0 +1,13 @@
+package com.curso.sprignboot.cursoSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSbApplication.class, args);
+	}
+
+}
