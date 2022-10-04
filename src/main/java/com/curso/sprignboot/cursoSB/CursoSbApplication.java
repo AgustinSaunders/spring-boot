38628b60ce559691a119764c2f56b9d59bd085fa
Agustin Saunders <saundersagustin94@gmail.com>
2022-10-03 23:45:34 -0300

    Fifth Commit

--- src/main/java/com/curso/sprignboot/cursoSB/CursoSbApplication.java
+++ src/main/java/com/curso/sprignboot/cursoSB/CursoSbApplication.java
@@ -1,9 +1,11 @@
 package com.curso.sprignboot.cursoSB;
 
+import com.curso.sprignboot.cursoSB.Pojo.UserPojo;
 import com.curso.sprignboot.cursoSB.bean.MyBean;
 import com.curso.sprignboot.cursoSB.bean.MyBeanWithDependency;
 import com.curso.sprignboot.cursoSB.bean.PropertiesBean;
 import com.curso.sprignboot.cursoSB.component.ComponentDependency;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,21 +13,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursoSbApplication implements CommandLineRunner {
+
+	private final org.apache.juli.logging.Log LOGGER = LogFactory.getLog(CursoSbApplication.class);
 	private MyBean myBean;
 	private PropertiesBean propertiesBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private ComponentDependency componentDependency;
+	private UserPojo userPojo;
 	//We have two classes which implements the same dependency
 	//So this will generate an error unless we specify which class we want to use
 	//For this we need to use the annotation @Qualifier
 	//followed by the name of the class we are going to use but in lower camel case
 	public CursoSbApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
 							  MyBean myBean, MyBeanWithDependency myBeanWithDependency,
-							  PropertiesBean propertiesBean){
+							  PropertiesBean propertiesBean, UserPojo userPojo){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.propertiesBean = propertiesBean;
+		this.userPojo = userPojo;
 	}
 
 
@@ -39,5 +45,7 @@ public class CursoSbApplication implements CommandLineRunner {
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
 		System.out.println(propertiesBean.function());
+		System.out.println(userPojo.getEmail() + "-" + userPojo.getAge());
+		LOGGER.error("FATAL ERROR, ALL YOUR INFORMATION WAS DELIVERED TO THE NSA");
 	}
 }
--- src/main/java/com/curso/sprignboot/cursoSB/configuration/GeneralConfiguration.java
+++ src/main/java/com/curso/sprignboot/cursoSB/configuration/GeneralConfiguration.java
@@ -1,12 +1,15 @@
 package com.curso.sprignboot.cursoSB.configuration;
 
+import com.curso.sprignboot.cursoSB.Pojo.UserPojo;
 import com.curso.sprignboot.cursoSB.bean.PropertiesBean;
 import com.curso.sprignboot.cursoSB.bean.PropertiesBeanImplements;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
