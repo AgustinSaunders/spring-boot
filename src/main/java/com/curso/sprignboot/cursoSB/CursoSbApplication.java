0da9da4f17d3a0f12edc5028a72f05ad7a018319
Agustin Saunders <saundersagustin94@gmail.com>
2022-10-18 00:07:20 -0300

    Sixth commit

--- src/main/java/com/curso/sprignboot/cursoSB/CursoSbApplication.java
+++ src/main/java/com/curso/sprignboot/cursoSB/CursoSbApplication.java
@@ -5,11 +5,18 @@ import com.curso.sprignboot.cursoSB.bean.MyBean;
 import com.curso.sprignboot.cursoSB.bean.MyBeanWithDependency;
 import com.curso.sprignboot.cursoSB.bean.PropertiesBean;
 import com.curso.sprignboot.cursoSB.component.ComponentDependency;
+import com.curso.sprignboot.cursoSB.entity.User;
+import com.curso.sprignboot.cursoSB.repository.UserRepository;
 import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class CursoSbApplication implements CommandLineRunner {
@@ -19,6 +26,7 @@ public class CursoSbApplication implements CommandLineRunner {
 	private PropertiesBean propertiesBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private ComponentDependency componentDependency;
+	private UserRepository userRepository;
 	private UserPojo userPojo;
 	//We have two classes which implements the same dependency
 	//So this will generate an error unless we specify which class we want to use
@@ -26,12 +34,13 @@ public class CursoSbApplication implements CommandLineRunner {
 	//followed by the name of the class we are going to use but in lower camel case
 	public CursoSbApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
 							  MyBean myBean, MyBeanWithDependency myBeanWithDependency,
-							  PropertiesBean propertiesBean, UserPojo userPojo){
+							  PropertiesBean propertiesBean, UserPojo userPojo, UserRepository userRepository){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.propertiesBean = propertiesBean;
 		this.userPojo = userPojo;
+		this.userRepository = userRepository;
 	}
 
 
@@ -41,6 +50,47 @@ public class CursoSbApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		//pastExamples();
+		saveUsersInDataBase();
+		getInformationJpqlFromUser();
+	}
+
+	private void getInformationJpqlFromUser(){
+		LOGGER.info("User with the method findByUserEmail" +
+				userRepository.findByUserEmail("minotaur@domain.com"));
+
+		userRepository.findAndSort("Asterio", Sort.by("id").descending())
+				.forEach(user -> LOGGER.info("User with sort method" + user));
+
+		userRepository.findByName("John")
+				.forEach(LOGGER::info);
+
+		LOGGER.info("The parameters you gave us thorow: " +
+		userRepository.getAllByBirthDateAndEmail(LocalDate.of(2017, 4, 25),
+				"jean@domain.com")
+				.orElseThrow(() -> new RuntimeException("User not found")));
+	}
+	private void saveUsersInDataBase(){
+		User user1 = new User("John", "john@domain.com",
+				LocalDate.of(2021, 3, 20));
+		User user2 = new User("Dominic", "dominic@domain.com",
+				LocalDate.of(2021, 1, 7));
+		User user3 = new User("Jean", "jean@domain.com",
+				LocalDate.of(2017, 4, 25));
+		User user4 = new User("Abelardo", "abelardo@domain.com",
+				LocalDate.of(2015, 12, 7));
+		User user5 = new User("Asterios", "minotaur@domain.com",
+				LocalDate.of(2004, 4, 30));
+		User user6 = new User("Asterios1", "youngminotaur@domain.com",
+				LocalDate.of(2004, 5, 1));
+		User user7 = new User("Asterios0", "oldminotaur@domain.com",
+				LocalDate.of(2004, 4, 29));
+
+		List<User> list = Arrays.asList(user1,user2, user3, user4, user5, user6, user7);
+		list.stream().forEach(userRepository::save);
+	}
+
+	private void pastExamples(){
 		componentDependency.greet();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
---
+++ src/main/java/com/curso/sprignboot/cursoSB/repository/UserRepository.java
@@ -0,0 +1,31 @@
+package com.curso.sprignboot.cursoSB.repository;
+
+import com.curso.sprignboot.cursoSB.dto.UserDto;
+import com.curso.sprignboot.cursoSB.entity.User;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email=?1")
+    Optional<User> findByUserEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.name LIKE ?1%")
+    List<User> findAndSort(String name, Sort sort);
+
+    List<User> findByName(String name);
+
+    @Query("SELECT new com.curso.sprignboot.cursoSB.dto.UserDto(u.id, u.name, u.birthDate) " +
+    "FROM User u " +
+    "WHERE u.birthDate=:parameterDate " +
+    "AND u.email=:parameterEmail")
+    Optional<UserDto> getAllByBirthDateAndEmail(@Param("parameterDate") LocalDate date,
+                                                @Param("parameterEmail") String email);
+}
