91ad953d3fbae25e0aa224837e3c4c19d10412ca
Agustin Saunders <saundersagustin94@gmail.com>
2022-10-18 23:57:55 -0300

    Last One

--- src/main/java/com/curso/sprignboot/cursoSB/controller/UserRestController.java
+++ src/main/java/com/curso/sprignboot/cursoSB/controller/UserRestController.java
@@ -5,6 +5,8 @@ import com.curso.sprignboot.cursoSB.caseUse.DeleteUser;
 import com.curso.sprignboot.cursoSB.caseUse.GetUser;
 import com.curso.sprignboot.cursoSB.caseUse.UpdateUser;
 import com.curso.sprignboot.cursoSB.entity.User;
+import com.curso.sprignboot.cursoSB.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,13 +21,16 @@ public class UserRestController {
     private CreateUser createUser;
     private DeleteUser deleteUser;
     private UpdateUser updateUser;
+    private UserRepository userRepository;
 
     public UserRestController(GetUser getUser, CreateUser createUser,
-                              DeleteUser deleteUser, UpdateUser updateUser) {
+                              DeleteUser deleteUser, UpdateUser updateUser,
+                              UserRepository userRepository) {
         this.getUser = getUser;
         this.createUser = createUser;
         this.deleteUser = deleteUser;
         this.updateUser = updateUser;
+        this.userRepository = userRepository;
     }
     @GetMapping("/")
     List<User> get(){
@@ -46,5 +51,12 @@ public class UserRestController {
     @PutMapping("/{id}")
     ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id){
        return new ResponseEntity<>(updateUser.update(newUser, id), HttpStatus.OK);
+    }
+
+    @GetMapping("/pageable")
+    List<User> getUserPageable(@RequestParam int page, @RequestParam int size){
+        return userRepository.findAll(PageRequest.of(page, size)).getContent();
+
+
     }
 }
